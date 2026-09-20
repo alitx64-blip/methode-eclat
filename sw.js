5752b347646faec2c51384c82519040c95f6bcea
alitx64-blip <alitx64@gmail.com>
2026-09-20 09:45:08 +0200

    Actualiser le cache de la conclusion

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'eclat-cache-v16';
+const CACHE_NAME = 'eclat-cache-v17';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=20260920-1',
-  './app.js?v=20260920-4',
+  './styles.css?v=20260920-2',
+  './app.js?v=20260920-5',
   './favicon.svg',
   './manifest.json'
 ];
