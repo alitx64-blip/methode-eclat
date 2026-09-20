35f15f36145cee3a4f9093be49e734250067a8fc
alitx64-blip <alitx64@gmail.com>
2026-09-20 10:28:33 +0200

    Finaliser le cache de l’expérience

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'eclat-cache-v20';
+const CACHE_NAME = 'eclat-cache-v21';
 const ASSETS = [
   './',
   './index.html',
   './styles.css?v=20260920-4',
-  './app.js?v=20260920-8',
+  './app.js?v=20260920-9',
   './favicon.svg',
   './manifest.json'
 ];
